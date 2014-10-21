0a577078de6d435b352580711b450a35d3cb91f7
yuya yano <yuya.yano.6260@gmail.com>
2014-10-21 05:09:00 +0000

    Modified run command for Heroku......

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ from memomemo import app, socketio
 from memomemo.database import init_db
 
 init_db()
-if 'CLEARDB_DATABASE_URL' in os.environ
-    socketio.run(app)
-else
-    socketio.run(app, host='0.0.0.0')
+
+host = '127.0.0.1'
+if 'CLEARDB_DATABASE_URL' in os.environ:
+    host='0.0.0.0'
+
+socketio.run(app, host=host)
